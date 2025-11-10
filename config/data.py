4eae5019e46375cad66c7023e59feb7abc4d00ec
DavidGR0788 <nelson.galvez@flyr.com>
2025-11-10 08:40:45 -0500

    Se crean carpetas para el caso 3 y asignar responsabilidades

---
+++ config/data.py
@@ -0,0 +1,11 @@
+# --- URLs del Proyecto (Simplificado) ---
+BASE_URL = "https://nuxqa3.avtest.ink/es/"
+
+# --- Credenciales para Caso 3 (Se mantienen aquí para cuando se usen) ---
+USERNAME_UATI = "21734198706"
+PASSWORD_UATI = "Lifemilesl"
+
+# --- Datos Fijos del Caso 3 ---
+LANGUAGE = "Francés"
+POS = "France"
+PASSENGER_COUNT = 3
